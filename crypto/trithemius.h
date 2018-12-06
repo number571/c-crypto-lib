efde2426fe4fc11be3fd61f09224790800e8fd5a
[#571] <31502293+Number571@users.noreply.github.com>
2018-12-07 02:35:14 +0700

    Update trithemius.h

--- crypto/trithemius.h
+++ crypto/trithemius.h
@@ -2,7 +2,7 @@
 
 extern char set_alpha_trithemius (const char * const alpha);
 extern char trithemius (
-	char * const to, 
+	char * to, 
 	const char mode, 
 	char (* const key) (const char x), 
 	char * const from
