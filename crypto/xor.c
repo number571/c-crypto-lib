1b3b2ec163220ce9662fb776e50aa683cd54002d
[#571] <31502293+Number571@users.noreply.github.com>
2018-12-07 01:17:34 +0700

    Delete xor.c

--- crypto/xor.c
+++
@@ -1,7 +0,0 @@
-#include "macro.h"
-
-extern void xor (char * to, const char key, const char * from) {
-    for (; *from != END_OF_STRING; ++from)
-        *to++ = *from ^ key;
-    *to = END_OF_STRING;
-}
