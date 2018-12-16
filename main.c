c15c76c71a1668496eabbbb9afffd354d8d9b552
Number571 <number571gg@mail.ru>
2018-12-16 21:51:50 +0700

    add main.c

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 #include "crypto/append/macro.h"
-#include "crypto/caesar_wordkey.h"
+#include "crypto/trithemius.h"
+
+char func_key(const char x) {
+	return x * 2 + 1;
+}
 
 int main(void) {
 	char message[100] = "HELLO, WORLD";
 
-	// set_alpha_caesar_wordkey("ABCDEFGHIJKLMNOPQRSTUVWXYZ, ");
+	set_alpha_trithemius("ABCDEFGHIJKLMNOPQRSTUVWXYZ, ");
 
-	caesar_wordkey(message, ENCRYPT_MODE, 3, "KNOWN", message);
+	trithemius(message, ENCRYPT_MODE, func_key, message);
 	printf("%s\n", message);
 
-	caesar_wordkey(message, DECRYPT_MODE, 3, "KNOWN", message);
+	trithemius(message, DECRYPT_MODE, func_key, message);
 	printf("%s\n", message);
 
 	return 0;
